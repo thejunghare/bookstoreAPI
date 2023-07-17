ea2b30d4904dbe2932c772a463f385b577b04df8
prasad-nimap <prasadjunghare@nimapinfotech.com>
2023-07-17 19:25:51 +0530

    db connection

---
+++ models/setup.go
@@ -0,0 +1,13 @@
+package models
+
+import "gorm.io/gorm"
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	database, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+}
