718968f374833617ba5e1977946d358a3cf97628
prasad-nimap <prasadjunghare@nimapinfotech.com>
2023-07-18 11:30:09 +0530

    connection and db and controllers

--- main.go
+++ main.go
@@ -4,14 +4,23 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/thejunghare/build/controllers"
+	"github.com/thejunghare/build/models"
 )
 
 func main() {
+	// Setting up the server
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+
+	models.ConnectDatabase() // Connect to database
+
+	r.GET("/books", controllers.FindBooks)
+
 	r.Run()
 }
--- models/setup.go
+++ models/setup.go
@@ -1,13 +1,27 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	database, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	// Connnect database
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to database!")
 	}
+
+	// Migrate the schema
+	// db.AutoMigrate(&Book{})
+	err = db.AutoMigrate(&Book{})
+	if err != nil {
+		return
+	}
+
+	// Used to get access to our database
+	DB = db
 }
